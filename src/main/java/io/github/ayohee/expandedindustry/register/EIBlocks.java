23bc4bee378090b5834318f114b1828ef35b2bcb
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-07-06 00:33:29 +0100

    Model and textures for erythrite. Datagen.

--- src/main/java/io/github/ayohee/expandedindustry/register/EIBlocks.java
+++ src/main/java/io/github/ayohee/expandedindustry/register/EIBlocks.java
@@ -1,7 +1,17 @@
 package io.github.ayohee.expandedindustry.register;
 
+import com.simibubi.create.foundation.data.SharedProperties;
+import com.tterrag.registrate.builders.ItemBuilder;
+import com.tterrag.registrate.providers.DataGenContext;
+import com.tterrag.registrate.providers.RegistrateItemModelProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import com.tterrag.registrate.util.nullness.NonNullFunction;
+import io.github.ayohee.expandedindustry.CreateExpandedIndustry;
 import io.github.ayohee.expandedindustry.ErythriteBlock;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 
 import static io.github.ayohee.expandedindustry.CreateExpandedIndustry.REGISTRATE;
 
@@ -11,8 +21,26 @@ public class EIBlocks {
     }
 
     public static final BlockEntry<ErythriteBlock> ERYTHRITE_BLOCK = REGISTRATE.block("erythrite", ErythriteBlock::new)
-            .simpleItem()
+            .initialProperties(SharedProperties::stone)
+            .item()
+            .transform(getItemModel())
             .register();
+
+    public static <I extends BlockItem, P> NonNullFunction<ItemBuilder<I, P>, P> getItemModel() {
+        return b -> b.model(EIBlocks::locateItemModel).build();
+    }
+
+    public static <I extends BlockItem> ItemModelBuilder locateItemModel(
+            DataGenContext<Item, I> ctx,
+            RegistrateItemModelProvider prov
+            ) {
+        String blockName = prov.name(ctx.getEntry()::getBlock);
+        return prov.withExistingParent(
+                blockName,
+                ResourceLocation.fromNamespaceAndPath(CreateExpandedIndustry.MODID, "item/" + blockName)
+        );
+    }
+
 
     public static void register() { }
 }
