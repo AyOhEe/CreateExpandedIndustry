015a9c95c4f5043c49d79b6ea39312211764f98b
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-07-12 15:51:02 +0100

    Patch structures can specify an origin distance range

--- src/main/java/io/github/ayohee/expandedindustry/content/structures/HardenedStonePatchStructures.java
+++ src/main/java/io/github/ayohee/expandedindustry/content/structures/HardenedStonePatchStructures.java
@@ -3,6 +3,7 @@ package io.github.ayohee.expandedindustry.content.structures;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.ayohee.expandedindustry.CreateExpandedIndustry;
 import io.github.ayohee.expandedindustry.register.EIStructures;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -31,7 +32,9 @@ public class HardenedStonePatchStructures extends Structure {
                     HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
                     Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
                     DimensionPadding.CODEC.optionalFieldOf("dimension_padding", JigsawStructure.DEFAULT_DIMENSION_PADDING).forGetter(structure -> structure.dimensionPadding),
-                    LiquidSettings.CODEC.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter(structure -> structure.liquidSettings)
+                    LiquidSettings.CODEC.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter(structure -> structure.liquidSettings),
+                    Codec.INT.fieldOf("min_origin_distance").forGetter(structure -> structure.minOriginDistance),
+                    Codec.INT.fieldOf("min_origin_distance").forGetter(structure -> structure.maxOriginDistance)
             ).apply(instance, HardenedStonePatchStructures::new));
 
     private final Holder<StructureTemplatePool> startPool;
@@ -41,6 +44,8 @@ public class HardenedStonePatchStructures extends Structure {
     private final int maxDistanceFromCenter;
     private final DimensionPadding dimensionPadding;
     private final LiquidSettings liquidSettings;
+    private final int minOriginDistance;
+    private final int maxOriginDistance;
 
     public HardenedStonePatchStructures(Structure.StructureSettings config,
                                         Holder<StructureTemplatePool> startPool,
@@ -49,7 +54,9 @@ public class HardenedStonePatchStructures extends Structure {
                                         HeightProvider startHeight,
                                         int maxDistanceFromCenter,
                                         DimensionPadding dimensionPadding,
-                                        LiquidSettings liquidSettings)
+                                        LiquidSettings liquidSettings,
+                                        int minOriginDistance,
+                                        int maxOriginDistance)
     {
         super(config);
         this.startPool = startPool;
@@ -59,17 +66,24 @@ public class HardenedStonePatchStructures extends Structure {
         this.maxDistanceFromCenter = maxDistanceFromCenter;
         this.dimensionPadding = dimensionPadding;
         this.liquidSettings = liquidSettings;
+        this.minOriginDistance = minOriginDistance;
+        this.maxOriginDistance = maxOriginDistance;
     }
 
-    private static boolean extraSpawningChecks(Structure.GenerationContext context) {
+    private boolean extraSpawningChecks(Structure.GenerationContext context) {
         ChunkPos chunkpos = context.chunkPos();
 
-        return context.chunkGenerator().getFirstOccupiedHeight(
-                chunkpos.getMinBlockX(),
-                chunkpos.getMinBlockZ(),
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                context.heightAccessor(),
-                context.randomState()) < 30;
+        int x = chunkpos.getMiddleBlockX();
+        int z = chunkpos.getMiddleBlockZ();
+        double originDistanceSqr = (x*x) + (z*z);
+        if (originDistanceSqr < minOriginDistance*minOriginDistance){
+            return false;
+        }
+        if (originDistanceSqr > maxOriginDistance*maxOriginDistance){
+            return false;
+        }
+
+        return true;
     }
 
     @Override
