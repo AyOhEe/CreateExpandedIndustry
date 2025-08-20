088017a1e438c3daed075d0c855c2f2fb2207955
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-08-20 17:39:14 +0100

    Cleanup. Improved textures.

--- src/main/java/io/github/ayohee/expandedindustry/content/blocks/FractionatingColumnPortBlock.java
+++ src/main/java/io/github/ayohee/expandedindustry/content/blocks/FractionatingColumnPortBlock.java
@@ -1,13 +1,10 @@
 package io.github.ayohee.expandedindustry.content.blocks;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.FluidState;
 
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.HORIZONTAL_FACING;
 
@@ -21,7 +18,7 @@ public class FractionatingColumnPortBlock extends Block {
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(HORIZONTAL_FACING);
     }
-    
+
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(HORIZONTAL_FACING, context.getHorizontalDirection());
