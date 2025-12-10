1ee9737fca0978ba47fe44ed1561274768ad2876
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-12-10 17:12:47 +0000

    Steam reforming recipe

--- src/main/java/io/github/ayohee/expandedindustry/datagen/recipes/EIColumnCrackingRecipeGen.java
+++ src/main/java/io/github/ayohee/expandedindustry/datagen/recipes/EIColumnCrackingRecipeGen.java
@@ -7,34 +7,46 @@ import io.github.ayohee.expandedindustry.register.EIFluids;
 import io.github.ayohee.expandedindustry.register.EIRecipeTypes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.material.Fluids;
 
 import java.util.concurrent.CompletableFuture;
 
 public class EIColumnCrackingRecipeGen extends StandardProcessingRecipeGen<ColumnCrackingRecipe> {
-    //TODO require hydrogen gas from steam reforming
-    //TODO steam reforming?
+    //TODO adjust numbers once recipes are implemented
+
     public final GeneratedRecipe KEROSENE_TO_RAW_LUBRICANT = create(
             "kerosene_to_raw_lubricant",
             b -> b.require(EIFluids.KEROSENE.get(), 250)
+                    .require(EIFluids.HYDROGEN_GAS.get(), 250)
                     .output(EIFluids.RAW_LUBRICATING_OIL.get(), 250)
     );
 
     public final GeneratedRecipe RAW_LUBRICANT_TO_NAPHTHA = create(
             "raw_lubricant_to_naphtha",
             b -> b.require(EIFluids.RAW_LUBRICATING_OIL.get(), 250)
+                    .require(EIFluids.HYDROGEN_GAS.get(), 250)
                     .output(EIFluids.NAPHTHA.get(), 250)
     );
 
     public final GeneratedRecipe NAPHTHA_TO_LPG = create(
             "naphtha_to_lpg",
             b -> b.require(EIFluids.NAPHTHA.get(), 250)
+                    .require(EIFluids.HYDROGEN_GAS.get(), 250)
                     .output(EIFluids.LIQUID_PETROLEUM_GAS.get(), 250)
     );
 
     public final GeneratedRecipe LPG_TO_ETHYLENE = create(
             "lpg_to_ethylene",
             b -> b.require(EIFluids.LIQUID_PETROLEUM_GAS.get(), 250)
+                    .require(EIFluids.HYDROGEN_GAS.get(), 250)
                     .output(EIFluids.ETHYLENE.get(), 250)
+    );
+
+    public final GeneratedRecipe STEAM_REFORMING = create(
+            "steam_reforming",
+            b -> b.require(EIFluids.LIQUID_PETROLEUM_GAS.get(), 250)
+                    .require(Fluids.WATER, 250)
+                    .output(EIFluids.HYDROGEN_GAS.get(), 250)
     );
 
     public EIColumnCrackingRecipeGen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
