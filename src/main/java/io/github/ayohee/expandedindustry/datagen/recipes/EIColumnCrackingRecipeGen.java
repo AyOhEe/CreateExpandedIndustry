9434eec9301314f3053dfea48ed7208099e883fa
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-12-10 16:43:47 +0000

    More column cracking recipes
    Holy shit the datagen just works. Incredible. Absolute cinema.

--- src/main/java/io/github/ayohee/expandedindustry/datagen/recipes/EIColumnCrackingRecipeGen.java
+++ src/main/java/io/github/ayohee/expandedindustry/datagen/recipes/EIColumnCrackingRecipeGen.java
@@ -4,7 +4,6 @@ import com.simibubi.create.api.data.recipe.StandardProcessingRecipeGen;
 import io.github.ayohee.expandedindustry.CreateExpandedIndustry;
 import io.github.ayohee.expandedindustry.content.recipe.ColumnCrackingRecipe;
 import io.github.ayohee.expandedindustry.register.EIFluids;
-import io.github.ayohee.expandedindustry.register.EIItems;
 import io.github.ayohee.expandedindustry.register.EIRecipeTypes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -12,6 +11,26 @@ import net.minecraft.data.PackOutput;
 import java.util.concurrent.CompletableFuture;
 
 public class EIColumnCrackingRecipeGen extends StandardProcessingRecipeGen<ColumnCrackingRecipe> {
+    //TODO require hydrogen gas from steam reforming
+    //TODO steam reforming?
+    public final GeneratedRecipe KEROSENE_TO_RAW_LUBRICANT = create(
+            "kerosene_to_raw_lubricant",
+            b -> b.require(EIFluids.KEROSENE.get(), 250)
+                    .output(EIFluids.RAW_LUBRICATING_OIL.get(), 250)
+    );
+
+    public final GeneratedRecipe RAW_LUBRICANT_TO_NAPHTHA = create(
+            "raw_lubricant_to_naphtha",
+            b -> b.require(EIFluids.RAW_LUBRICATING_OIL.get(), 250)
+                    .output(EIFluids.NAPHTHA.get(), 250)
+    );
+
+    public final GeneratedRecipe NAPHTHA_TO_LPG = create(
+            "naphtha_to_lpg",
+            b -> b.require(EIFluids.NAPHTHA.get(), 250)
+                    .output(EIFluids.LIQUID_PETROLEUM_GAS.get(), 250)
+    );
+
     public final GeneratedRecipe LPG_TO_ETHYLENE = create(
             "lpg_to_ethylene",
             b -> b.require(EIFluids.LIQUID_PETROLEUM_GAS.get(), 250)
